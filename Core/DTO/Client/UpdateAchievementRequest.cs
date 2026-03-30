c0815de17650b4aa84a07b943f139ef619923f62
unknown <brunomendestk@outlook.com.br>
2026-03-30 14:30:43 -0300

    NotificationAjustepushhkjjyhasddsasddadas

--- Core/DTO/Client/UpdateAchievementRequest.cs
+++ Core/DTO/Client/UpdateAchievementRequest.cs
@@ -7,7 +7,6 @@ namespace Core.DTO.Client
         [StringLength(200, ErrorMessage = "Título deve ter no máximo 200 caracteres")]
         public string? Title { get; set; }
 
-        [StringLength(500, ErrorMessage = "Descrição deve ter no máximo 500 caracteres")]
         public string? Description { get; set; }
 
         public string? Type { get; set; }
